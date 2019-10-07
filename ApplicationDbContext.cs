4d6ba2baba3979c428f530563e4597717249a5ec
rexcole97 <54370507+rexcole97@users.noreply.github.com>
2019-10-07 14:57:06 -0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using baebook1.Data;
+
+namespace baebook1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<baebook1.Data.Class> Class { get; set; }
+    }
+}
---
+++ Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace baebook1.Data
+{
+    public class Class
+    {
+        [Key]
+        public int puppyid { get; set; }
+        public string breed { get; set; }
+        public string sex { get; set; }
+        public double price { get; set; }
+         
+    }
+}
